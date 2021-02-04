500c7ae4b9440d8f47011c37f6439740742cf8c4
NicklasNorgaard <kontakt@nicklasnorgaard.org>
2021-02-04 12:37:12 +0100

    git init

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<div class="footer">
+    <div>
+        <a href="#">link1</a>
+        <a href="#">link2</a>
+        <a href="#">link3</a>
+    </div>
+    <div>
+        <a href="#">link4</a>
+        <a href="#">link5</a>
+        <a href="#">link6</a>
+    </div>
+    <div>
+        <a href="#">link7</a>
+        <a href="#">link8</a>
+        <a href="#">link9</a>
+    </div>
+</div>
---
+++ includes/nav.php
@@ -0,0 +1,7 @@
+<div class="nav">
+    <a href="index.php">forside</a>
+    <a href="#">om os</a>
+    <a href="kontakt.php">kontakt</a>
+    <a href="#">Punkt</a>
+</div>
+
